2030d60e3e0fdb8aab3667897a081b2c14d24310
dusmith0 <128092158+dusmith0@users.noreply.github.com>
2023-03-16 15:59:49 -0500

    Update Random Sample of Albatros Activity.r

--- Random Sample of Albatros Activity.r
+++ Random Sample of Albatros Activity.r
@@ -1,3 +1,9 @@
+### This code is used to generate random samples from a real study of albatross chicks. We use it in class to generate random bootlegged samples of the population,
+### and create confedence intervals of the weight loss during incubation. 
+
+### The code can be improved by adding an output pdf or xlsx file with the data instead of imputing it derectly. Also the values are hard coded and changed at the beginning of 
+### the code. 
+
 n = 30
 beginmean = 3565
 incmean = 3107
